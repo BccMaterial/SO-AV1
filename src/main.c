c63bad3f22a02b2ba39da52e10334aa7101f68fd
resutemon1 <resutemon@gmail.com>
2025-10-14 22:26:16 -0300

    add abre arquivo e mostra tamanho

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
 
+// criar uma função em C para cada coisa que fiz 
 int main() {
-	printf("Hello, World!");
+  int file_size;
+  unsigned char *dados_imagem;
+  char *nome_arquivo = "imagens/casa-geometria.bmp";
+  FILE *file_pointer = NULL;
+	// Abre o arquivo
+  file_pointer = fopen(nome_arquivo, "rb");
+  // Se deu erro ao abrir, retorna
+  if (file_pointer == NULL) {
+    printf("Erro ao abrir o arquivo: %i", errno);
+    return 1;
+  }
+  fseek(file_pointer, 0L, SEEK_END);
+  // Pega o índice do ponteiro
+  	file_size = ftell(file_pointer);
+  	printf("O tamanho do arquivo é de %i bytes\n", file_size);
+    // 3. Aloca memória para o array de bytes
+    //dados_imagem = (unsigned char *)malloc(file_size * sizeof(unsigned char));
+
+    //free(dados_imagem);
 	return 0;
 }
